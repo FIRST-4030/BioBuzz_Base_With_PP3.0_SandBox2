1aa432acc9ecf35aaa953e363d7939b2bedaf086
Dennis O'Brien <dco6453@me.com>
2026-09-21 11:14:22 -0700

    Added meat to the 'create' method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,11 +1,18 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.algorithm.Foresight;
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.revhub.localizers.PinpointLocalizer;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static Follower create(HardwareMap h) {
-        // return new Follower(Drivetrain, Localizer, Foresight);
+//        return new Follower(
+//                new PinpointLocalizer(h, localizerConfig),
+//                new Mecanum(h, drivetrainConfig),
+//                new Foresight(foresightConfig)
+//        );
         return null;
     }
 }
